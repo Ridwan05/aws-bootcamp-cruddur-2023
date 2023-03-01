85340094e97b05531940450ba036c9446793e782
Ridwan Oladele <ridohlah74@gmail.com>
2023-03-01 16:26:21 +0000

    update gitpod.yml

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -22,7 +22,6 @@ from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 # Recorder
 xray_url = os.getenv("AWS_XRAY_URL")
 xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
-XRayMiddleware(app, xray_recorder)
 
 # HoneyComb--------------
 # app.py updates
@@ -48,6 +47,9 @@ trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
+
+# X-RAY -------
+XRayMiddleware(app, xray_recorder)
 
 # HoneyComb--------------
 # Initialize automatic instrumentation with Flask
